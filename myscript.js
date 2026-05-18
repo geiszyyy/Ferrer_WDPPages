804f99c2cb24afec01649c1f1b9eb50ec2fc9339
geiszyyy <geiszyferrer.30@gmail.com>
2026-05-18 08:53:53 +0800

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,8 @@
+var myHeading = document.querySelector('h1');
+myHeading.innerHTML = 'Hello WOrld!';
+
+var myHeading = document.querySelector('h2');
+myHeading.innerHTML = 'Tama po noh?';
+
+var myHeading = document.querySelector('h3');
+myHeading.innerHTML = 'PEDE NA PO KUMAIN?';
